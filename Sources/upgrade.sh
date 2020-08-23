1ffbea91851b813e52a64ece8e8ced331cfe1ebb
Jorge Ivan <59676765+Leviatansan21@users.noreply.github.com>
2020-08-23 11:15:12 -0500

    Upgrade

--- Sources/upgrade.sh
+++ Sources/upgrade.sh
@@ -1,1 +1,13 @@
+echo "Windows Load files"
+./boot.wim
+echo "Searching Previo Version iconpack"
+find
+echo " Deleting old Version"
+mv usr/share/icons/"Windows 7 Ultimate"
+sleep 3s
+echo "Upgrading Windows
+tar -xfv ./install.wim
+echo "Continue Installing..."
+fc-cache
+done
 
